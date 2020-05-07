d16c96f9217f611f7018f4a8647de3c8611e6ba8
Mariela Vallecillo <mvalletelt@gmail.com>
2020-05-07 16:01:47 -0300

    subo ejercicio de la carta y encuesta Resto

---
+++ Zotros/link a git.js
@@ -0,0 +1,29 @@
+https://github.com/0220CBFSNCN01ARMZ/node01/tree/master/archivos-practica-html-css
+
+
+https://meyerweb.com/eric/tools/css/reset/
+
+<h3>Entradas</h3>
+
+<strong>Muzzarella Sticks</strong>
+<p>Bastones de muzzarella rebozados</p>
+
+
+<h4>Muzzarella Sticks</h4>
+                    <p>Bastones de muzzarella rebozados</p>
+
+<h3>Entradas</h3>
+
+<strong>Muzzarella Sticks</strong>
+<p>Bastones de muzzarella rebozados</p>
+video clase romota formulario:
+
+https://zoom.us/rec/play/65B4Jbiprjo3GtaSuQSDA_J_W9TpeKOs0ShL86AOz0bjAiZXZ1XzZbdAYeSx1oUY5cPUVoMGBTvlmgzh?continueMode=true&_x_zm_rtaid=emmJgdevR7C6naH0jK2KEA.1588419767829.054a54cb465a55f1129f035e0b29823a&_x_zm_rhtaid=596
+
+htmlSup.net
+
+material.io
+
+getbootstrap.com
+
+html5up.net
